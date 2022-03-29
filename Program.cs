3cb465bd46cf2815390a38850c0d99fe1c63cb5b
sabrina-reis1 <sabrinaramos440@gmail.com>
2022-03-29 17:55:52 -0300

    Adicionando a situação do usuário

--- Program.cs
+++ Program.cs
@@ -19,6 +19,35 @@ namespace calculadoraIMC
 
             Console.WriteLine("{0}, sua altura é {1} e seu peso é {2}", nome, altura, peso);
             Console.WriteLine("Seu IMC é {0}", imc);
+
+            if(imc < 17.00) 
+            {
+                Console.WriteLine("Você está muito abaixo do peso.");
+            } 
+            else if (imc >=17 && imc < 18.50)
+            {
+                 Console.WriteLine("Você está abaixo do peso.");
+            }
+            else if (imc >= 18,50 && imc < 25)
+            {
+                Console.WriteLine("Você está no peso ideal.");
+            }
+            else if(imc >= 25 && imc < 30)
+            { 
+                Console.WriteLine("Você está acima do peso.");
+            }
+            else if(imc >=30 && imc < 35) 
+            {
+                Console.WriteLine("Situação: Obesidade I");
+            }
+            else if(imc >=35 && imc < 40) 
+            {
+                Console.WriteLine("Situação: Obesidade II (severa)");
+            }
+            else
+            {
+                Console.WriteLine("Situação: Obesidade III (mórbida)");
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Aplicação dotnet console para calculo do IMC.
 
 ## Funcionalidades
 - Calcula o IMC
+- Apresenta a situação do usuário
 
 ## Tecnologias
 - dotnet core 2.1
