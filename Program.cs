ec4eb800cbe92c01d1bd10f86951d1f67e2ac7ca
Sabrina Ramos <sabrinaramos440@gmail.com>
2022-04-03 16:36:44 -0300

    informando a quantidade de Kg a perder ou ganhar

--- Program.cs
+++ Program.cs
@@ -17,36 +17,47 @@ namespace calculadoraIMC
 
             double imc = peso / (altura * altura);
 
-            Console.WriteLine("{0}, sua altura é {1} e seu peso é {2}", nome, altura, peso);
+            Console.WriteLine("{0}, sua altura é {1} e seu peso é {2} Kg", nome, altura, peso);
             Console.WriteLine("Seu IMC é {0}", imc);
 
             if(imc < 17.00) 
             {
                 Console.WriteLine("Você está muito abaixo do peso.");
             } 
-            else if (imc >=17 && imc < 18.50)
+            else if (imc >=17.00 && imc < 18.50)
             {
                  Console.WriteLine("Você está abaixo do peso.");
             }
-            else if (imc >= 18,50 && imc < 25)
+            else if (imc >= 18.50 && imc < 25.00)
             {
                 Console.WriteLine("Você está no peso ideal.");
             }
-            else if(imc >= 25 && imc < 30)
+            else if(imc >= 25.00 && imc < 30.00)
             { 
                 Console.WriteLine("Você está acima do peso.");
             }
-            else if(imc >=30 && imc < 35) 
+            else if(imc >=30.00 && imc < 35.00) 
             {
                 Console.WriteLine("Situação: Obesidade I");
             }
-            else if(imc >=35 && imc < 40) 
+            else if(imc >=35.00 && imc < 40.00) 
             {
                 Console.WriteLine("Situação: Obesidade II (severa)");
             }
             else
             {
                 Console.WriteLine("Situação: Obesidade III (mórbida)");
+            }
+
+            double abaixoPesoIdeal = 18.50 * (altura * altura);
+            double acimaPesoIdeal = 24.99 * (altura * altura);
+
+            if(peso <= abaixoPesoIdeal)
+            {
+                Console.WriteLine("Você precisa ganhar {0} Kg para alcançar o peso 'ideal'", abaixoPesoIdeal - peso);
+            }
+            else if(peso >= acimaPesoIdeal) {
+                Console.WriteLine("Você precisa perder {0} Kg para alcançar o peso 'ideal'", peso - acimaPesoIdeal);
             }
         }
     }
--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Aplicação dotnet console para calculo do IMC.
 ## Funcionalidades
 - Calcula o IMC
 - Apresenta a situação do usuário
+- A partir da situação do usuário, informa o quanto o mesmo deve perder ou ganhar para alcançar o peso "ideal".
 
 ## Tecnologias
 - dotnet core 2.1
